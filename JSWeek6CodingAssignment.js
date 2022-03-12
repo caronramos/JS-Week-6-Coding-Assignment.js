7a4d9a12398c0edb331072095f4daaf92c1628ad
Caron Ramos <98505172+caronramos@users.noreply.github.com>
2022-03-12 12:11:01 -0800

    Update JSWeek6CodingAssignment.js

--- JSWeek6CodingAssignment.js
+++ JSWeek6CodingAssignment.js
@@ -14,6 +14,33 @@ console.log(c.showCard());
 class Deck {
   constructor(){
     let cards = [
+        //fill the deck with Card objects of the
+        //appropriate suit and value
       ];
+  createDeck(){
+    cards.push(new Card("spades", 1));
+    
+  }
+  
+}
+
+class Player {
+  constructor(name){
+    this.name = name;
+}
+  
+showPlayer() {
+  return this.name;
+}
+  
+class Game {
+  constructor(){
+    
+  }
+  startGame(){
+    
+  }
+  compareScore(){
+    
   }
 }
