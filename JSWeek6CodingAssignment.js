bb574de0b4139440641c5e717951785636b5b13f
Caron Ramos <98505172+caronramos@users.noreply.github.com>
2022-03-12 08:10:52 -0800

    Update JSWeek6CodingAssignment.js

--- JSWeek6CodingAssignment.js
+++ JSWeek6CodingAssignment.js
@@ -1,1 +1,15 @@
+class Card {
+  constructor(suit, value){
+    this.suit = suit;
+    this.value = value;
+}
 
+showCard(){
+  return `Suit: ${this.suit}, Value: ${this.value}`;
+}
+
+let c = new Card("clubs", 9);
+console.log(c.showCard());
+
+class Deck {
+  constructor(){
